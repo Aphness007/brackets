569d6c4b591fe242f6ea529a0335b9b5f09d6bef
aphness007 <aphness007@gmail.com>
2022-08-08 11:02:24 +0300

    feat:solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   if(str.length % 2 !== 0){
-    return false
+      return false
+    }
+
+  let brackets = "";
+  let newStr = str;
+  for (let i = 0; i < str.length / 2; i++) {
+    for (let y = 0; y < bracketsConfig.length; y++) {
+      brackets = bracketsConfig[y][0] + bracketsConfig[y][1];
+      newStr = newStr.replace(brackets, "");
+    }
   }
-  return true;
-}
+
+  return newStr.length == 0;
+};
+  // if(str.length % 2 !== 0){
+  //   return false
+  // }
+  // return true;
+// }
